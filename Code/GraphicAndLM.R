dd9879b3e9c1acd0c0050e1988444b464f7152da
SofiaArreaga99 <sigreduccion@gmail.com>
2025-06-23 02:23:56 -0400

    Add project organization to directory

--- Code/GraphicAndLM.R
+++ Code/GraphicAndLM.R
@@ -5,14 +5,14 @@ library(ggplot2)
 
 # data frame
 datos <- data.frame(
-  Año = c(1970, 1981, 1991, 2001, 2010, 2020),
+  Year = c(1970, 1981, 1991, 2001, 2010, 2020),
   MgC = c(83.18, 73.74, 78.75, 87.27,94.16, 112),
   MinError = c(82.35, 72.85, 77.67, 86.05,92.82,109.61),
   MaxError = c(84.01, 74.63, 79.83, 88.49, 95.5, 112.79)
 )
 
 
-ggplot(datos, aes(x = Año, y = MgC)) +
+ggplot(datos, aes(x = Year, y = MgC)) +
   geom_line(color = "#2C3E50", size = 1) +  # Línea entre puntos
   geom_point(size = 3, color = "#E67E22", shape = 20, fill = "#E67E22") +  # Puntos
   geom_errorbar(aes(ymin = MinError, ymax = MaxError), width = 0.5, color = "#34495E", size = 1) +  # Barras de error 
@@ -34,7 +34,7 @@ ggplot(datos, aes(x = Año, y = MgC)) +
 
 
 
-model <- lm(MgC ~ Año, data = datos)
+model <- lm(MgC ~ Year, data = datos)
 summary(model)
 
 
@@ -42,7 +42,7 @@ summary(model)
 ## Graphic of the linear model ##
 
 
-ggplot(datos, aes(x = Año, y = MgC)) +
+ggplot(datos, aes(x = Year, y = MgC)) +
   geom_point(size = 4, color = "#E67E22", shape = 20, fill = "#E67E22") +  # Puntos
   geom_errorbar(aes(ymin = MinError, ymax = MaxError), width = 0.2, color = "#34495E", size = 1.5) +  # Barras de error
   geom_smooth(method = "lm", se = TRUE, color = "#2C3E50", fill = "lightblue", alpha = 0.3) +  
